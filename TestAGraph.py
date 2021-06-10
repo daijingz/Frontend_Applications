43c508b86edb6ec8ad9c3081a827da117e7f0b0a
Jingze Dai <61481741+daijingz@users.noreply.github.com>
2021-06-10 15:33:29 -0400

    Add files via upload

--- TestAGraph.py
+++ TestAGraph.py
@@ -1,9 +1,13 @@
+# Author: Jingze Dai
+# Date: 16/02/2021
+# Email Address: daij24@mcmaster.ca or david1147062956@163.com
 import unittest
 import Approximation_Graph
 
 
 class MyTestCase(unittest.TestCase):
     def test_FindVertexCover(self):
+        """! Test Approximation_graph programs' getting-vertex-cover methods"""
         G1 = Approximation_Graph.Approximation_Graph()
         G1.addEdge(0, 1)
         G1.addEdge(1, 2)
@@ -26,3 +30,20 @@ class MyTestCase(unittest.TestCase):
         G3.addEdge(1, 4)
         G3.addEdge(4, 3)
         self.assertEqual(G3.FindVertexCover(), [0, 3])
+
+        G4 = Approximation_Graph.Approximation_Graph()
+        G4.addEdge(0, 1)
+        G4.addEdge(1, 2)
+        G4.addEdge(2, 3)
+        G4.addEdge(1, 0)
+        G4.addEdge(2, 1)
+        G4.addEdge(3, 2)
+        self.assertEqual(G4.FindVertexCover(), [0, 2])
+
+        G5 = Approximation_Graph.Approximation_Graph()
+        G5.addEdge(0, 1)
+        G5.addEdge(1, 4)
+        G5.addEdge(4, 3)
+        G5.addEdge(3, 2)
+        G5.addEdge(2, 1)
+        self.assertEqual(G5.FindVertexCover(), [0, 2, 4])
