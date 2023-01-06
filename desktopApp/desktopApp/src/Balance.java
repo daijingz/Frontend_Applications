eba78e9fa96445cf7487fd8f37365ecb63c4b6a8
Jingze Dai <61481741+daijingz@users.noreply.github.com>
2023-01-05 23:33:31 -0500

    Add files via upload

--- desktopApp/desktopApp/src/Balance.java
+++ desktopApp/desktopApp/src/Balance.java
@@ -2,14 +2,16 @@ class Balance {
     private String user_name;
     private double chequering_balance;
     private double saving_balance;
+    private int limit;
 
-    public Balance(String user_name) throws Exception {
+    public Balance(String user_name, int limit) throws Exception {
         if (user_name.length() < 2){
             throw new Exception();
         }
         this.user_name = user_name;
         this.chequering_balance = 0.0;
         this.saving_balance = 0.0;
+        this.limit = limit;
     }
 
     public void set_user_name(String new_user_name) throws Exception {
@@ -37,5 +39,16 @@ class Balance {
 
     public double get_saving_balance(){
         return this.saving_balance;
+    }
+
+    public void set_limit(int new_limit) throws Exception {
+        if (new_limit < 500){
+            throw new Exception();
+        }
+        this.limit = new_limit;
+    }
+
+    public int get_limit(){
+        return this.limit;
     }
 }
